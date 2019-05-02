efcb44b69c5c6ce8b5b8142fcdf123d7ebfd3423
Unknown <connor.mcnutt.2022@student.lhschools.org>
2019-05-02 13:47:10 -0400

    added first bit of code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,26 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Testing");
+        int input;
+        boolean victory;
+
+
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+	System.out.println("Welcome to my game of Rock Paper Scissors.");
+	System.out.println("Do you choose Rock [1], Paper [2], or Scissors [3]?");
+	input=keyboard.nextInt();
+
+
+
     }
+
+
+    
 }
