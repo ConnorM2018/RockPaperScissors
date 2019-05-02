9d1dcffc3db57bbef35122ae5497688bb1ea62b8
Unknown <connor.mcnutt.2022@student.lhschools.org>
2019-05-02 13:56:41 -0400

    Added a program that determines what an input means, and started on another program that decides whether the user wins or not

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,18 +9,60 @@ public class Main {
         boolean victory;
 
 
-
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
 	System.out.println("Welcome to my game of Rock Paper Scissors.");
 	System.out.println("Do you choose Rock [1], Paper [2], or Scissors [3]?");
 	input=keyboard.nextInt();
-
+	System.out.println("You chose "+choiceSpeaker(input)+"!");
+	victory=victoryChooser(input);
 
 
     }
 
 
-    
+    public static boolean victoryChooser(int input){
+
+        int choice;
+        choice=(int)(Math.random()*100);
+        while(choice>3||choice==0){
+            choice=(int)(Math.random()*100);
+        }
+        System.out.println("I choose "+choice+"!");
+
+        switch(input){
+            case 1:
+                switch(choice){
+                    case 1:
+                    case 2:
+                    case 3:
+                }
+            case 2:
+                switch(choice){
+                    case 1:
+                    case 2:
+                    case 3:
+                }
+            case 3:
+                switch(choice){
+                    case 1:
+                    case 2:
+                    case 3:
+                }
+
+        }
+
+
+    }
+    public static String choiceSpeaker(int input){
+        switch(input){
+            case 1:
+                return "Rock";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+        }
+    }
 }
