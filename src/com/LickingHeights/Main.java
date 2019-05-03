09c0ae522ed24d111fbc2f547d432f3d62bac6e8
ConnorM2018 <42775777+ConnorM2018@users.noreply.github.com>
2019-05-03 11:21:59 -0400

    Update Main.java
    Added a subprogram that will make the user re-type their input if they type anything that isn't 1, 2, or 3. Also cleaned up the code and renamed a few things.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,24 +5,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to my game of Rock Paper Scissors.");
         mainGame();
     }
+    
+    
     public static void mainGame(){
         int input;
 
 
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-
-        System.out.println("Welcome to my game of Rock Paper Scissors.");
+        
+        
         System.out.println("Do you choose Rock [1], Paper [2], or Scissors [3]?");
         input=keyboard.nextInt();
+        
+        
+        if(input>3||input<1){
+            invalidInput();
+        }
+        
         System.out.println("_________________________");
         System.out.println("You chose "+choiceSpeaker(input)+"!");
-        victoryChooser(input);
+        winOrLose(input);
     }
 
-    public static void victoryChooser(int input){
+    public static void winOrLose(int input){
 
         int choice;
         choice=(int)(Math.random()*100);
@@ -30,7 +39,7 @@ public class Main {
             choice=(int)(Math.random()*100);
         }
         System.out.println("I choose "+choiceSpeaker(choice)+"!");
-        System.out.println("_________________________");
+        System.out.println(" ");
 
         switch(input){
             case 1:
@@ -72,8 +81,8 @@ public class Main {
                         break;
                 }
                 break;
-
         }
+        System.out.println("_________________________");
         mainGame();
 
 
@@ -87,6 +96,10 @@ public class Main {
             case 3:
                 return "Scissors";
         }
-        return "to doom us all";
+        return "[ERROR]";
+    }
+    public static void invalidInput(){
+        System.out.println("That is not a valid response. Please try again.");
+        mainGame();
     }
 }
